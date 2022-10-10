544e7f93caf3acef8d0d8da8e4c101ab9b09f22d
tiger <thisisstreamliner@gmail.com>
2022-10-11 00:11:44 +0300

    Remove counting score when hit object

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -8,9 +8,10 @@ public class ObjectHit : MonoBehaviour
 
     private void OnCollisionEnter(Collision other) 
     {
-        if (other.gameObject.tag == "Player") 
+        if (other.gameObject.CompareTag("Player")) 
         {
             GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
         }
     }
     
--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -9,7 +9,10 @@ public class Scorer : MonoBehaviour
     // Start is called before the first frame update
     void OnCollisionEnter(Collision other)
     {
-        hits++;
-        print("You've bumped into a thing this many times: " + hits);
+        if (!other.gameObject.CompareTag("Hit"))
+        {
+            hits++;
+            Debug.Log("You've bumped into a thing this many times: " + hits);
+        }
     }
 }
